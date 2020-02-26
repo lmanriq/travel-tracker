b0f023e40df771179a3cb6d802e7e4d97a07b8a3
Lili Manrique <liliana.manrique1@gmail.com>
2020-02-26 12:27:37 -0700

    Write log in function

--- src/classes/Traveler.js
+++ src/classes/Traveler.js
@@ -5,7 +5,25 @@ class Traveler {
     this.travelerType = null;
   }
 
-
+  logIn(username, password, userData) {
+    let valid = true;
+    let regex = /customer(?:[1-9]|[1-4][0-9]|50)$/
+    const validPassword = 'travel2020';
+    if (regex.test(username) && password === validPassword) {
+      this.id = parseInt(username.slice(7));
+      const matchedTraveler = userData.find(user => user.id === this.id);
+      this.name = matchedTraveler.name;
+      this.travelerType = matchedTraveler.travelerType;
+      return 'success'
+    } else if (username === 'agency' && password === validPassword) {
+      this.id = 0;
+      this.name = 'Agent Lopez';
+      this.travelerType = 'manager';
+      return 'success'
+    } else {
+      return 'Incorrect username or password. Please try again.'
+    }
+  }
 }
 
 export default Traveler;
