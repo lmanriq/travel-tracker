8324a7cbf05163e30b1824b10427508acf237039
Lili Manrique <liliana.manrique1@gmail.com>
2020-02-26 18:27:27 -0700

    Write constructor and calculate cost methods

--- src/classes/Trip.js
+++ src/classes/Trip.js
@@ -1,7 +1,41 @@
 class Trip {
-  constructor() {
+  constructor(userID, destinationID, numTravelers, date, duration) {
+    this.id = Date.now();
+    this.userID = userID;
+    this.destinationID = destinationID;
+    this.travelers = numTravelers;
+    // <string 'YYYY/MM/DD'>
+    this.date = date;
+    this.duration = duration;
+    this.status = 'pending';
+    this.suggestedActivities: []
+  }
+
+  getDestinationDetails(destinationData) {
+    return destinationData.find(destination => destination.id === this.destinationID);
+  }
+
+  calculateCostBreakdown(destinationData) {
+    const destination = this.getDestinationDetails(destinationData);
+    const flightCost = this.travelers * destination.estimatedFlightCostPerPerson;
+    const lodgingCost = this.travelers * this.duration * destination.estimatedLodgingCostPerDay;
+    const serviceFee = (flightCost + lodgingCost) * .1;
+    const totalCost = flightCost + lodgingCost + serviceFee;
+    return {
+      flightCost,
+      lodgingCost,
+      serviceFee,
+      totalCost
+    }
+  }
+
+  delete() {
+
+  }
+
+  sortByStatus() {
 
   }
 }
 
-export default Trip; 
+export default Trip;
