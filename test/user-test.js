ddb4ee9c6ab3bb9161a41eaf01731c16658860a7
Lili Manrique <liliana.manrique1@gmail.com>
2020-02-27 15:49:41 -0700

    Comment out spies since that functionality has been moved

--- test/user-test.js
+++ test/user-test.js
@@ -3,21 +3,21 @@ import spies from 'chai-spies';
 import sampleTravelers from '../src/sample-data/sample-traveler-data';
 import User from '../src/classes/User';
 
-chai.use(spies);
+// chai.use(spies);
 
 describe('User', function() {
   let travelerData;
   let user;
-  global.window = {};
+  // global.window = {};
 
   beforeEach(function() {
     travelerData = sampleTravelers.travelers;
     user = new User();
-    chai.spy.on(window, 'fetch', () => new Promise((resolve, reject) => {}));
+    // chai.spy.on(window, 'fetch', () => new Promise((resolve, reject) => {}));
   });
 
   afterEach(function() {
-    chai.spy.restore();
+    // chai.spy.restore();
   });
 
   it('should be a function', function() {
@@ -42,13 +42,13 @@ describe('User', function() {
 
   describe('logging in', function() {
     it('should be able to log in (traveler)', function() {
-      user.logIn('traveler1', 'travel2020');
-      expect(window.fetch).to.be.called(1);
-      expect(window.fetch).to.be.called.with('https://fe-apps.herokuapp.com/api/v1/travel-tracker/1911/travelers/travelers/1')
+      expect(user.logIn('traveler1', 'travel2020')).to.eq(1);
+      // expect(window.fetch).to.be.called(1);
+      // expect(window.fetch).to.be.called.with('https://fe-apps.herokuapp.com/api/v1/travel-tracker/1911/travelers/travelers/1')
     });
 
     it('should be able to log in (agency)', function() {
-      user.logIn('agency', 'travel2020', travelerData);
+      user.logIn('agency', 'travel2020');
       expect(user.id).to.eq(0);
       expect(user.name).to.eq('Agent Barbarita Lopez');
       expect(user.travelerType).to.eq('agent');
