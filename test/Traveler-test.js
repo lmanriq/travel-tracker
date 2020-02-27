d2dfb20386551891f02ae04597640aabe8500ff5
Lili Manrique <liliana.manrique1@gmail.com>
2020-02-27 11:20:31 -0700

    Add tests for cost method and add chai spies for requestTrip

--- test/Traveler-test.js
+++ test/Traveler-test.js
@@ -1,8 +1,12 @@
-import chai from 'chai';
-const expect = chai.expect;
+import chai, { expect } from 'chai';
+import spies from 'chai-spies';
 import sampleTravelers from '../src/sample-data/sample-traveler-data';
+import { BASE, TRIPS_ENDPOINT } from '../src/constants/constants';
 import User from '../src/classes/User';
 import Traveler from '../src/classes/Traveler';
+import Trip from '../src/classes/Trip';
+
+chai.use(spies);
 
 describe('Traveler', function() {
   let travelerData;
@@ -18,6 +22,14 @@ describe('Traveler', function() {
     }
     user = new User(sampleUser);
     traveler = new Traveler(user);
+    global.window;
+    global.localStorage;
+    chai.spy.on(window, 'fetch', () => new Promise((resolve, reject) => {}));
+    chai.spy.on(localStorage, ['getItem', 'setItem'], () => {});
+  });
+
+  afterEach(function() {
+    chai.spy.restore();
   });
 
   it('should be a function', function() {
@@ -44,36 +56,28 @@ describe('Traveler', function() {
     expect(traveler.myTrips).to.deep.eq([]);
   });
 
-  it('should instantiate without money spent', function() {
-    expect(traveler.amountSpent).to.eq(0);
+  describe('request trips', function() {
+    let newTrip;
+
+    beforeEach(function() {
+      traveler.requestTrip(2, 5, '2021/12/15', 5);
+      newTrip = new Trip (traveler.id, 2, 5, '2021/12/15', 5);
+    });
+
+    it('should send trip requests to the server', function() {
+      expect(window.fetch).to.be.called(1);
+      expect(window.fetch).to.be.called.with(BASE + TRIPS_ENDPOINT, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(newTrip)
+      })
+    });
+
+    it('should save its trips to local storage', function() {
+      expect(localStorage.setItem).to.be.called(1);
+      expect(localStorage.setItem).to.be.called.with('myTrips', JSON.stringify(newTrip));
+    });
   });
-  //
-  // describe('logging in', function() {
-  //   it('should be able to log in', function() {
-  //     expect(traveler.logIn('traveler1', 'travel2020', travelerData)).to.eq('success');
-  //   });
-  //
-  //   it('should not be able to log in with an incorrect username', function() {
-  //     expect(traveler.logIn('travel1', 'travel2020', travelerData)).to.eq('Incorrect username or password. Please try again.');
-  //   });
-  //
-  //   it('should not be able to log in with an incorrect password', function() {
-  //     expect(traveler.logIn('traveler1', 'travel2021', travelerData)).to.eq('Incorrect username or password. Please try again.');
-  //   });
-  //
-  //   it('should have an ID after logging in', function() {
-  //     traveler.logIn('traveler1', 'travel2020', travelerData)
-  //     expect(traveler.id).to.eq(1);
-  //   });
-  //
-  //   it('should initialize without a name', function() {
-  //     traveler.logIn('traveler1', 'travel2020', travelerData)
-  //     expect(traveler.name).to.eq('Ham Leadbeater');
-  //   });
-  //
-  //   it('should initialize without a traveler type', function() {
-  //     traveler.logIn('traveler1', 'travel2020', travelerData)
-  //     expect(traveler.travelerType).to.eq('relaxer');
-  //   });
-  // });
 });
