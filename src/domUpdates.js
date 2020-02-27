2f5b62bc36694ad353f38ee551436f35025451fb
Lili Manrique <liliana.manrique1@gmail.com>
2020-02-27 14:39:10 -0700

    Split login functionality between index and dom docs

--- src/domUpdates.js
+++ src/domUpdates.js
@@ -1,8 +1,36 @@
 import $ from 'jquery';
+import Traveler from './classes/Traveler';
+import TravelAgency from './classes/TravelAgency';
+import { BASE, TRAVELER_ENDPOINT } from './constants/constants';
+import {
+  getUserInfo
+} from './index';
+
 
 const dom = {
+  bindLoginButton(state) {
+    $('.btn--login').on('click', null, state, getUserInfo)
+  },
+
+  bindEvents(state) {
+    // $('.btn--login').on('click', null, state, dom.loginUser)
+  },
+
+  loadDashboard(state) {
+    $('.login-header').text(state.currentUser.name);
+  },
+
   loadLogin() {
     $('.login-header').hide().fadeIn(3000);
+  },
+
+  loginUser() {
+    const username = $('#username-input').val();
+    const password = $('#password-input').val();
+    return {
+      username,
+      password
+    }
   }
 }
 
--- src/index.js
+++ src/index.js
@@ -4,6 +4,10 @@
 // An example of how you import jQuery into a JS file if you use jQuery in that file
 import $ from 'jquery';
 import dom from './domUpdates';
+import User from './classes/User';
+import Traveler from './classes/Traveler';
+import TravelAgency from './classes/TravelAgency';
+import { BASE, TRAVELER_ENDPOINT } from './constants/constants';
 
 // An example of how you tell webpack to use a CSS (SCSS) file
 import './css/base.scss';
@@ -11,6 +15,40 @@ import './css/base.scss';
 // An example of how you tell webpack to use an image (also need to link to it in the index.html)
 import './images/plane-icon.svg'
 
+const state = {
+  currentUser: new User()
+}
+
 dom.loadLogin();
+// dom.bindEvents(state);
+export const getUserInfo = (e) => {
+  console.log('sup')
+  const credentials = dom.loginUser();
+  const login = state.currentUser.logIn(credentials.username, credentials.password, e.data)
+  if (login === 0) {
+    state.currentUser = new TravelAgency(state.currentUser);
+    dom.loadDashboard(state);
+  } else if (login > 0) {
+    window.fetch(BASE + TRAVELER_ENDPOINT + login)
+      .then(response => {
+        console.log(response.status)
+        return response.json()
+      })
+      .then(data => data)
+      .then(data => {
+        console.log(data.name)
+        state.currentUser = new Traveler(data);
+        dom.loadDashboard(state)
+      })
+      .catch(error => {
+        console.log(error.message)
+      })
+  } else {
+    $('#incorrect-login-alert').text(login).hide().fadeIn(1000).delay(1000).fadeOut(1000);
+  }
+}
+
+dom.bindLoginButton(state) ;
+
 
 console.log('This is the JavaScript entry file - your code begins here.');
