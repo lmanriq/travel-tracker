a1fcec9323390ac8ed5b823b0f0a061e47a3f52c
Lili Manrique <liliana.manrique1@gmail.com>
2020-02-26 17:40:30 -0700

    Wireframe html

---
+++ src/constants/constants.js
@@ -0,0 +1,4 @@
+export const BASE = 'https://fe-apps.herokuapp.com/api/v1/travel-tracker/1911/';
+export const TRAVELER_ENDPOINT = 'travelers/travelers/';
+export const TRIPS_ENDPOINT = 'trips/trips/';
+export const DESTINATIONS_ENDPOINT = 'destinations/destinations/';
