e2153db05d1bca9caa87d007b1db827cfb8d1a78
Lili Manrique <liliana.manrique1@gmail.com>
2020-02-27 19:10:51 -0700

    Add update endpoint

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,4 +1,5 @@
 export const BASE = 'https://fe-apps.herokuapp.com/api/v1/travel-tracker/1911/';
 export const TRAVELER_ENDPOINT = 'travelers/travelers/';
 export const TRIPS_ENDPOINT = 'trips/trips/';
+export const TRIPS_MODIFICATION_ENDPOINT = 'trips/updateTrip/';
 export const DESTINATIONS_ENDPOINT = 'destinations/destinations/';
