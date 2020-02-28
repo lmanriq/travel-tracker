911312bd3e091cbec63d5184831bc69a96aec78d
Lili Manrique <liliana.manrique1@gmail.com>
2020-02-27 19:10:12 -0700

    Add traveler search method

--- src/classes/TravelAgency.js
+++ src/classes/TravelAgency.js
@@ -1,4 +1,7 @@
+import { BASE, TRIPS_MODIFICATION_ENDPOINT } from '../constants/constants';
 import User from '../classes/User';
+import Trip from '../classes/Trip';
+import Traveler from '../classes/Traveler';
 
 class TravelAgency extends User {
   constructor(user) {
@@ -8,12 +11,50 @@ class TravelAgency extends User {
     // this.travelerType = user ? user.travelerType : null;
   }
 
-  approveRequest() {
-
+  approveRequest(tripID) {
+    const update = {
+      id: tripID,
+      status: 'approved'
+    }
+    window
+      .fetch(BASE + TRIPS_MODIFICATION_ENDPOINT, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(update)
+      })
+      .then(response => {
+        console.log(response.status)
+        return response.json()
+      })
+      .then(data => data)
+      .catch(error => {
+        throw error
+      }
+    )
   }
 
-  denyRequest() {
-    //Send the delete request
+  denyRequest(tripID, tripData) {
+    const targetTrip = new Trip(tripData.find(trip => trip.id === tripID));
+    targetTrip.delete();
+  }
+
+  searchForTraveler(travelerName, travelerData, tripData, destinationData) {
+    let targetTraveler = travelerData.find(traveler => traveler.name === travelerName);
+    if (targetTraveler) {
+      targetTraveler = new Traveler(targetTraveler, tripData);
+      return targetTraveler;
+    } else {
+      return 'User not found'
+    }
+  }
+
+  calculateTotalRevenue(travelerData, destinationData) {
+    const allTravelers = travelerData.map(traveler => new Traveler(traveler));
+    return allTravelers.reduce((total, traveler) => {
+      return total + traveler.calculateTotalAmountSpent(destinationData);
+    }, 0);
   }
 }
 
