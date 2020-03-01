61fe4be070b15dbe7f645c896d2668ff29ee2460
Lili Manrique <liliana.manrique1@gmail.com>
2020-02-29 23:42:35 -0700

    Start travel agent dash modifications and add search functions

--- src/domUpdates.js
+++ src/domUpdates.js
@@ -29,6 +29,15 @@ const dom = {
     });
   },
 
+  bindAgentBtns(state) {
+    $('.btn--pending').on('click', function() {
+      dom.showPendingTrips(state);
+    });
+    $('.btn--all').on('click', function() {
+      dom.displayTrips(state);
+    });
+  },
+
   showCostBreakdown(e) {
     let trip = dom.grabTripDetails(e.data);
     if (trip) {
@@ -71,6 +80,28 @@ const dom = {
     dom.addDatePicker();
     dom.addDestinationOptions(state);
     dom.bindDashBtns(state);
+  },
+
+  loadTravelAgent(state) {
+    dom.loadDashboard(state);
+    dom.showCurrentTravelers(state);
+    dom.displayTravelerSearch(state);
+    dom.displayTrips(state);
+    dom.displayTotalRevenue(state);
+    dom.bindAgentBtns(state)
+  },
+
+  showCurrentTravelers(state) {
+    const currentTravelers = state.currentUser.showCurrentTrips(state.trips).length;
+    $('.welcome-banner').append(`<h2>current wanderers: ${currentTravelers}</h2>`)
+  },
+
+  displayTravelerSearch(state) {
+    $('#side-header').text('search all wanderers');
+    const searchForm = `<label for="traveler-search">name:</label>
+      <input id="traveler-search" type="text">
+      <button class="btn btn--search" type="button" name="search">search</button>`
+    $('.trip-form').html(searchForm);
   },
 
   grabTripDetails(state) {
@@ -130,27 +161,34 @@ const dom = {
     </article>`
   },
 
+  displayTotalRevenue(state) {
+    let amount = state.currentUser.calculateTotalRevenue(state.travelers, state.destinations, state.trips);
+    amount = amount.toFixed(2);
+    amount = amount.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+    $('#amount-spent').text(amount);
+  },
+
   displayAmountSpentTraveler(state) {
     let amount = state.currentUser.calculateTotalAmountSpent(state.destinations);
+    amount = amount.toFixed(2);
     amount = amount.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
     $('#amount-spent').text(amount)
   },
 
   displayTrips(state) {
-    const data = state;
-    const currentTrips = data.currentUser.showCurrentTrips(data.trips);
-    const pastTrips = data.currentUser.showPastTrips(data.trips);
-    const futureTrips = data.currentUser.showFutureTrips(data.trips);
+    const currentTrips = state.currentUser.showCurrentTrips(state.trips);
+    const pastTrips = state.currentUser.showPastTrips(state.trips);
+    const futureTrips = state.currentUser.showFutureTrips(state.trips);
     const currentTripCards = currentTrips.map(trip => {
-      const destination = data.destinations.find(destination => destination.id === trip.destinationID);
+      const destination = state.destinations.find(destination => destination.id === trip.destinationID);
       return dom.makeTripCard(trip, destination, 'current');
     })
     const futureTripCards = futureTrips.map(trip => {
-      const destination = data.destinations.find(destination => destination.id === trip.destinationID);
+      const destination = state.destinations.find(destination => destination.id === trip.destinationID);
       return dom.makeTripCard(trip, destination)
     })
     const pastTripCards = pastTrips.map(trip => {
-      const destination = data.destinations.find(destination => destination.id === trip.destinationID);
+      const destination = state.destinations.find(destination => destination.id === trip.destinationID);
       return dom.makeTripCard(trip, destination)
     })
     $('.current-trips').html(currentTripCards.join('') + futureTripCards.join(''));
